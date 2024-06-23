efb7cd258078e24cff831d09702b6685e85a4071
Carlos <cassylenoft@gmail.com>
2024-06-23 13:30:34 -0600

    git ignore modified

---
+++ chat/english_version/load_objects.py
@@ -0,0 +1,10 @@
+import pickle, joblib
+
+with open('/home/ackerman/Proyect-Movie-Recomendations-System-/chat/english_version/objects/vectorizer.pkl','rb') as file:
+    vectorizer = pickle.load(file)
+#vectorizer= pickle.load(open('../objects/vectorizer.pkl','rb'))
+modelo_prediction = joblib.load('/home/ackerman/Proyect-Movie-Recomendations-System-/chat/english_version/models/english_model.joblib')
+encoder = pickle.load(open('/home/ackerman/Proyect-Movie-Recomendations-System-/chat/english_version/objects/encoder.pkl','rb'))
+df = pickle.load(open('/home/ackerman/Proyect-Movie-Recomendations-System-/recomendation_model/pickle_objects_saved/df.pkl','rb'))
+symilarity = pickle.load(open('/home/ackerman/Proyect-Movie-Recomendations-System-/recomendation_model/pickle_objects_saved/similarity.pkl','rb'))
+
---
+++ chat/english_version/procces_entry.py
@@ -0,0 +1,67 @@
+from load_objects import vectorizer, modelo_prediction, encoder, df
+#[['title','type','cast','genres_reduction','description']]
+import re
+def get_prediction(text):
+    matrix = vectorizer.transform([text]).toarray()
+    prediction = modelo_prediction.predict(matrix.reshape(1,-1))
+    prediction_encoded = encoder.inverse_transform(prediction)
+    category = prediction_encoded
+    category = only_letters(str(category))
+    
+    return category
+
+def show_random_movies(category):
+    if category in ['old','recent']:
+        if category == 'old':
+            random_movies = df[df[category] == 1].sample(n=3)
+            return random_movies[['title','genres_reduction']]
+        else:
+            random_movies = df[df[category] == 0].sample(n=3)
+            return random_movies[['title','genres_reduction']]
+
+    elif category in ['short','long']:
+        if category == 'short':
+            random_movies = df[df[category] == 1].sample(n=3)
+            return random_movies[['title','genres_reduction']]
+        else:
+            random_movies = df[df[category] == 0].sample(n=3)
+            return random_movies[['title','genres_reduction']]
+
+    elif category in ['local','foregin']:
+        if category == 'foregin':
+            random_movies = df[df[category] == 1].sample(n=3)
+            return random_movies[['title','genres_reduction']]
+        else:
+            random_movies = df[df[category] == 0].sample(n=3)
+            return random_movies[['title','genres_reduction']]
+
+    elif category in  ['action',
+       'anime', 'classic', 'comedy', 'documental', 'drama', 'family',
+       'fantasy', 'foregin', 'lgbt', 'music', 'religion', 'romance', 'show',
+       'sports', 'suspense', 'short', 'movie']:
+        for name in ['action',
+       'anime', 'classic', 'comedy', 'documental', 'drama', 'family',
+       'fantasy', 'foregin', 'lgbt', 'music', 'religion', 'romance', 'show',
+       'sports', 'suspense', 'short', 'movie']:
+            if category == name:
+                random_movies = df[df[category] == 1].sample(n=3)
+        return random_movies[['title','genres_reduction']]
+    else:
+        print('sorry category not avaliable')
+
+def only_letters(input_string):
+    # Patrón regex para encontrar solo letras (mayúsculas y minúsculas)
+    patron = re.compile(r'[^a-zA-Z]+')
+    
+    # Aplicar el patrón para limpiar el string
+    resultado = patron.sub('', input_string)
+    return resultado
+
+if __name__ == "__main__":
+    texto = input('ingresa un texto: ')
+    category = get_prediction(texto)
+    print(type(category))
+    print('here youve some to watch for your category: {}'.format(category))
+    df_random = show_random_movies(category=category)
+    print(df_random)
+    
