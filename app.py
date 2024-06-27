6715421a7baa883fe66d8f963b583f048712fc45
Carlos <cassylenoft@gmail.com>
2024-06-27 17:25:26 -0600

    checkpoint before javascript

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import (Flask, request, render_template, make_response, redirect)
 from chat.english_version import procces_entry
+import json
 #from procces_entry import get_prediction, show_random_movies
-app = Flask(__name__, template_folder='./template')
+app = Flask(__name__, template_folder='./template',static_folder='./css',)
+with open('./data/moives_home.json') as file:
+        home=json.load(file)
+
 
 @app.route('/')
 def hello():
     initial_bot_ms = 'write whatever\nyou want to watch'
-    return render_template('chatgpt.html',initial_bot_ms=initial_bot_ms)
+    
+    return render_template('home.html',initial_bot_ms=initial_bot_ms,home=home)
 
 @app.route('/chat',methods=['GET','POST'])
 def get_text():
@@ -16,9 +21,15 @@ def get_text():
     category,f1 = get_bot_response()
     respuesta = {'categoria': category,
                 'movie1':f1,
+                'imagen_url':"/css/images/alien_poster.jpg"
             }
-    return render_template('response2.html', **respuesta)
+    return render_template('response_3.html', **respuesta)
     #return 'el texto ingresado es: {}; la respuesta del bote es {}'.format(texto,respuesta)
+
+
+@app.route('/view')
+def show():
+     return 
 
 def get_bot_response():
     texto = request.form.get('texto','')
@@ -27,22 +38,6 @@ def get_bot_response():
 
     return category, f1
 
-    
-# def index():
-#     return render_template('chat.html', resultado="")
-
-# @app.route('/procesar', methods=['POST'])
-# def procesar():
-#     texto_ingresado = request.form['texto']
-#     # Aquí puedes agregar la lógica para obtener un comentario del bot según el texto ingresado
-#     comentario_bot = obtener_comentario_bot(texto_ingresado)  # Define esta función según tu necesidad
-#     resultado = f"Texto ingresado: {texto_ingresado}. {comentario_bot}"
-#     return render_template('chat.html', resultado=resultado)
-
-# def obtener_comentario_bot(texto):
-#     # Esta función puede ser modificada según la lógica que quieras implementar
-#     # Aquí se puede simular un comentario del bot basado en el texto ingresado
-#     return "Aquí obtendrás un comentario del bot basado en el texto ingresado."
 
 if __name__ == " __main__":
     app.run(debug=True)
