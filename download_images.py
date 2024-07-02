a729a23b644d0d9c8cfd8f3e4be1f16f8fdff9c7
Carlos <cassylenoft@gmail.com>
2024-07-02 14:48:25 -0600

    y

--- download_images.py
+++ download_images.py
@@ -9,13 +9,13 @@ def get_images(df):
     bin_searchs = [titles[i] + ' ' + type[i] + ' ' + str(year[i]) + ' poster' for  i in range(0,len(titles))] 
     images_dir = []
     for name in bin_searchs:
-        path = Path(f'/css/images/bing_images/{name}')
+        path = Path(f'css/images/bing_images/{name}')
         if path.is_dir():
             print('el directorio existe no se descargo nada')
             continue
         else:
             print('el directorio no existia se creo uno y se descargo la imagen')
             downloader.download(name,limit=1,
-                                adult_filter_off=True, force_replace=False, timeout=60,output_dir='/home/ackerman/Proyect-Movie-Recomendations-System-/css/images/bing_images/')
+                                adult_filter_off=True, force_replace=False, timeout=60,output_dir='css/images/bing_images/')
             images_dir.append(str(path)+'/Image_1.jpg')
     return images_dir
